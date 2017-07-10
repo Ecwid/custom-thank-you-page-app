4d22711fb1132fdc24e2f5d823b642714595eedd
rick <rick@ecwid.com>
2017-07-10 17:07:39 +0400

    Updates for accessibility support (jpdevries)

--- app-files/thankYouIframe.js
+++ app-files/thankYouIframe.js
@@ -12,7 +12,6 @@ var initialConfig = {
 };
 
 // For getting store information from Ecwid REST API
-
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -21,42 +20,41 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-// Executes when we have a new user install the app. It creates and sets the default data using Ecwid JS SDK and Application storage
 
+// Executes when we have a new user install the app. It creates and sets the default data using Ecwid JS SDK and Application storage
 function createUserData() {
 
-var theUrl = 'https://app.ecwid.com/api/v3/' + storeId + '/profile?token=' + accessToken;
+	var theUrl = 'https://app.ecwid.com/api/v3/' + storeId + '/profile?token=' + accessToken;
 
-var storeProfile = httpGet(theUrl);
-storeProfile = JSON.parse(storeProfile);
+	var storeProfile = httpGet(theUrl);
+	storeProfile = JSON.parse(storeProfile);
 
-initialConfig.storeUrl = storeProfile.generalInfo.storeUrl;
-initialConfig.enabled = false;
-initialConfig.delay = 0;
+	initialConfig.storeUrl = storeProfile.generalInfo.storeUrl;
+	initialConfig.enabled = false;
+	initialConfig.delay = 0;
 
 
-var data = '{"storeUrl": "'+ initialConfig.storeUrl + '", "enabled": '+ initialConfig.enabled +', "delay":'+ initialConfig.delay +'}';
+	var data = '{"storeUrl": "'+ initialConfig.storeUrl + '", "enabled": '+ initialConfig.enabled +', "delay":'+ initialConfig.delay +'}';
 
-EcwidApp.setAppPublicConfig(data, function(){
-	console.log('Public config saved!');
-});
+	EcwidApp.setAppPublicConfig(data, function(){
+		console.log('Public config saved!');
+	});
 
-document.getElementById('storeUrl').value = initialConfig.storeUrl;
-document.getElementById('delay').value = initialConfig.delay;
-document.getElementById('enabled').checked = initialConfig.enabled;
-document.querySelector("tr:nth-child(2)").style.visibility = 'hidden';
+	document.getElementById('storeUrl').value = initialConfig.storeUrl;
+	document.getElementById('delay').value = initialConfig.delay;
+	document.getElementById('enabled').checked = initialConfig.enabled;
+	document.querySelector("tr:nth-child(2)").style.visibility = 'hidden';
 
-// Setting flag to determine that we already created and saved defaults for this user
+	// Setting flag to determine that we already created and saved defaults for this user
+	var appExists = {
+		exists: 'yes'
+	};
 
-var appExists = {
-	exists: 'yes'
-};
+	console.log(data);
 
-console.log(data);
-
-EcwidApp.setAppStorage(appExists, function(){
-  console.log('Data saved!');
-});
+	EcwidApp.setAppStorage(appExists, function(){
+	  console.log('Data saved!');
+	});
 
 }
 
@@ -64,7 +62,6 @@ EcwidApp.setAppStorage(appExists, function(){
 
 
 // Executes if we have a user who logs in to the app not the first time. We load their preferences from Application storage with Ecwid JS SDK and display them in the app iterface
-
 function getUserData() {
 
 	EcwidApp.getAppStorage('public', function(config){
@@ -77,60 +74,59 @@ function getUserData() {
 		console.log(loadedConfig);
 	});
 
-setTimeout(function(){
+	setTimeout(function(){
 
-	document.getElementById('storeUrl').value = loadedConfig.storeUrl;
-	document.getElementById('storeUrl').disabled = !loadedConfig.enabled;
-	document.getElementById('delay').value = loadedConfig.delay;
-	document.getElementById('enabled').checked = loadedConfig.enabled;
+		document.getElementById('storeUrl').value = loadedConfig.storeUrl;
+		document.getElementById('storeUrl').disabled = !loadedConfig.enabled;
+		document.getElementById('delay').value = loadedConfig.delay;
+		document.getElementById('enabled').checked = loadedConfig.enabled;
 
-	if (loadedConfig.enabled) {
-		document.querySelector("tr:nth-child(2)").style.visibility = 'visible';
-	} else {
-		document.querySelector("tr:nth-child(2)").style.visibility = 'hidden';
-	}
+		if (loadedConfig.enabled) {
+			document.querySelector("tr:nth-child(2)").style.visibility = 'visible';
+		} else {
+			document.querySelector("tr:nth-child(2)").style.visibility = 'hidden';
+		}
 
-}, 1000);
+	}, 1000);
 
 }
+
 
 
 
 // Executes when Save button is pressed. Gets all elements' values and saves them to Application storage via Ecwid JS SDK
-
 function saveUserData() {
 
-var d = document.getElementById("save");
-d.className += " btn-loading";
+	var d = document.getElementById("save");
+	d.className += " btn-loading";
 
-setTimeout(function(){
-	d.className = "btn btn-primary btn-large";
-},500)
+	setTimeout(function(){
+		d.className = "btn btn-primary btn-large";
+	},500)
 
-var saveData = {
-	storeUrl: loadedConfig.storeUrl,
-	enabled: loadedConfig.enabled,
-	delay: loadedConfig.delay
-}
+	var saveData = {
+		storeUrl: loadedConfig.storeUrl,
+		enabled: loadedConfig.enabled,
+		delay: loadedConfig.delay
+	}
 
-saveData.storeUrl = document.getElementById('storeUrl').value;
-saveData.enabled = document.getElementById('enabled').checked;
-saveData.delay = parseInt(document.getElementById('delay').value);
+	saveData.storeUrl = document.getElementById('storeUrl').value;
+	saveData.enabled = document.getElementById('enabled').checked;
+	saveData.delay = parseInt(document.getElementById('delay').value);
 
-if (isNaN(saveData.delay)) {
-	saveData.delay = 0;
-}
+	if (isNaN(saveData.delay)) {
+		saveData.delay = 0;
+	}
 
-var dataToSave = '{"storeUrl": "'+ saveData.storeUrl + '", "enabled": '+ saveData.enabled +', "delay":'+ saveData.delay +'}';
+	var dataToSave = '{"storeUrl": "'+ saveData.storeUrl + '", "enabled": '+ saveData.enabled +', "delay":'+ saveData.delay +'}';
 
-EcwidApp.setAppPublicConfig(dataToSave, function(){
-	console.log('Public config saved!');
-});
+	EcwidApp.setAppPublicConfig(dataToSave, function(){
+		console.log('Public config saved!');
+	});
 
 }
 
 // Main app function to determine if the user is new or just logs into the app
-
 EcwidApp.getAppStorage('exists', function(value){
 
   if (value != null) {
--- app-files/thankYouStorefront.js
+++ app-files/thankYouStorefront.js
@@ -1,13 +1,9 @@
-
 // Get app public config value from Ecwid
-
 var customThankYouConfig = Ecwid.getAppPublicConfig('custom-thank-you');
 customThankYouConfig = JSON.parse(customThankYouConfig);
-
-//console.log(customThankYouConfig);
+console.log(customThankYouConfig);
 
 // If the app is enabled in storefront
-
 if(customThankYouConfig.enabled == true) {
 
     if (customThankYouConfig.storeUrl.substring(0,7) == "http://" || customThankYouConfig.storeUrl.substring(0,8) == "https://") {
@@ -17,14 +13,12 @@ if(customThankYouConfig.enabled == true) {
         customThankYouConfig.storeUrl = "//" + customThankYouConfig.storeUrl;
     }
 
-// If Thank you page is opened, redirect customer to set URL in the app
-    
     if (
       typeof(Ecwid) == 'object'
       && typeof(Ecwid.OnPageLoad) == 'object'
     ) {
       Ecwid.OnPageLoad.add(function(page) {
-        
+        // If Thank you page is opened, redirect customer to set URL in the app
         if (
           typeof(page) == 'object'
           && (page.type == 'ORDER_CONFIRMATION' || page.type == 'CHECKOUT_RESULT') // if offline or online payment method was used
